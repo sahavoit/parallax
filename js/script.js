eae800956c0b3cfd92123c1cb981015488f0f9e6
sahavoit <106018287+sahavoit@users.noreply.github.com>
2022-05-25 13:28:21 +0300

    parallax

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*$(document).ready(function() {
+	$('.header__burger').click(function() {
+		$('header__burger,.header_menu').toggleClass('active');
+	});
+});*/
+jQuery(document).ready(function (){
+	
+	$(".parallax_list>li").addClass('layer');
+	$('.parallax_list').parallax();
+	$(".wrapper").addClass('active');
+			
+			
+});
+
+
+	
